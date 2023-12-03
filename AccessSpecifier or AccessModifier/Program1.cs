9f5464fc4c53965d2a2fd5449015433f508a446b
Muhammad bilal <meharbilal24525@gmail.com>
2023-12-03 13:04:42 +0500

    Add project files.

---
+++ AccessSpecifier or AccessModifier/Program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AccessSpecifier_or_AccessModifier
+{
+    public class class1
+    {
+        public void show1()
+        {
+            Console.WriteLine("This is Base class method Show1");
+        }
+        //public void show2()
+        //{
+        //    //ya is trha
+        //    //show1();
+        //    //ya dosri trha method k andar method
+        //    class1 c1 = new class1();
+        //    c1.show1();
+
+        //}
+    
+    }
+    class Program1 : class1
+    {
+        static void Main(string[] args)
+        {
+            Program1 p1 = new Program1();
+            p1.show1();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AccessSpecifier or AccessModifire2/Program2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AccessSpecifier_or_AccessModifier;
+
+namespace AccessSpecifier_or_AccessModifire2
+{
+    class Program2:class1
+    {
+        static void Main(string[] args)
+        {
+            Program2 p2 = new Program2();
+            p2.show1();
+            Console.ReadLine();
+        }
+    }
+}
